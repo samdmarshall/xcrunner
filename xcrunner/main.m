c8905ef75cce3fe3e8a6b5d825c245540ac09102
Samantha Marshall <me@samdmarshall.com>
2015-10-01 19:56:00 -0400

    updating to not use hardcoded path

--- xcrunner/main.m
+++ xcrunner/main.m
@@ -2,20 +2,89 @@
 //  main.m
 //  xcrunner
 //
-//  Created by Sam Marshall on 12/25/14.
-//  Copyright (c) 2014 Sam Marshall. All rights reserved.
+//  Created by Samantha Marshall on 12/25/14.
+//  Copyright (c) 2014 Samantha Marshall. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#include <sys/stat.h>
+
+int 	allpaths;
+char *foundPath;
+
+static int is_there(char *candidate)
+{
+	struct stat fin;
+	
+	/* XXX work around access(2) false positives for superuser */
+	bool access_ok = (access(candidate, X_OK) == 0);
+	bool stat_ok = (stat(candidate, &fin) == 0);
+	bool isreg_ok = (S_ISREG(fin.st_mode));
+	bool file_exists = (access_ok && stat_ok && isreg_ok);
+	
+	bool is_not_root = (getuid() != 0);
+	bool stat_mode_ok = ((fin.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) != 0);
+	
+	bool found_match = (file_exists && (is_not_root || stat_mode_ok));
+	if (found_match) {
+		foundPath = candidate;
+	}
+	return found_match;
+}
+
+static int print_matches(char *path, char *filename)
+{
+	char candidate[PATH_MAX];
+	const char *d;
+	int found = 0;
+	
+	if (strchr(filename, '/') != NULL) {
+		found = is_there(filename);
+	}
+	
+	if (found == 0) {
+		while ((d = strsep(&path, ":")) != NULL && found == 0) {
+			if (*d == '\0') {
+				d = ".";
+			}
+			
+			if (snprintf(candidate, sizeof(candidate), "%s/%s", d, filename) >= (int)sizeof(candidate)) {
+				continue;
+			}
+			
+			found = is_there(candidate);
+		}
+	}
+	
+	return (found ? 0 : -1);
+}
+
+NSString * GetPath()
+{
+	NSString *xcrunPath;
+	
+	char *p = getenv("PATH");
+	NSString *pathVar = [NSString stringWithFormat:@"%s",p];
+	
+	if (print_matches((char *)[pathVar UTF8String], "xcrun") == 0) {
+		xcrunPath = [NSString stringWithFormat:@"%s", foundPath];
+	}
+	else {
+		NSLog(@"ERROR! Could not find xcrun!");
+		exit(0);
+	}
+	
+	return xcrunPath;
+}
 
 
-
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
 	@autoreleasepool {
 		NSArray *args = [[NSProcessInfo processInfo] arguments];
 		
 		NSTask *sdkDirTask = [[NSTask alloc] init];
-		[sdkDirTask setLaunchPath:@"/usr/bin/xcrun"];
+		[sdkDirTask setLaunchPath:GetPath()];
 		
 		NSInteger sdkIndex = [args indexOfObject:@"-sdk"];
 		if (sdkIndex != NSNotFound) {
@@ -52,7 +121,7 @@ int main(int argc, const char * argv[]) {
 		
 		if (![fullSDKDirPath isEqualToString:@""]) {
 			NSTask *xcrunTask = [[NSTask alloc] init];
-			[xcrunTask setLaunchPath:@"/usr/bin/xcrun"];
+			[xcrunTask setLaunchPath:GetPath()];
 			
 			NSMutableDictionary *newEnv = [[[NSProcessInfo processInfo] environment] mutableCopy];
 			
